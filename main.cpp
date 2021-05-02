4e29324d66ce3e76d3441e6dce01b256adfc7c5a
ZoeyF75 <zf.zoodle@gmail.com>
2021-05-01 21:17:44 -0700

    Variables with C++

--- main.cpp
+++ main.cpp
@@ -4,6 +4,13 @@ using namespace std;
 
 int main ()
 {
-  cout << "Hello world!" << endl;
+  string characterName = "John";
+  int characterAge;
+  characterAge = 35;
+  cout << "There once was a man named " << characterName << endl;
+  cout << "He was " << characterAge << " years old" << endl;
+  cout << "He liked the name " << characterName << endl;
+  cout << "He did not being " << characterAge << endl;
+
   return 0;
 }
