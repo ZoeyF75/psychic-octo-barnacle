776f8b3efa21ea8f1f0fd3c9254920a4ac0033fd
ZoeyF75 <zf.zoodle@gmail.com>
2021-05-01 21:31:12 -0700

    Data types

---
+++ dataTypes.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+  char grad = 'A';
+  string phrase = "Giraffe Academy";
+  int age = 50;
+  float num = 1.5;
+  // double can have more decimal places
+  double gpa = 2.3;
+  bool isMale = false;
+
+  return 0;
+}
