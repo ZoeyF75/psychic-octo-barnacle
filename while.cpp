8ba191cfda0ff6644aea2226b0a6bfeb5eccbfc2
ZoeyF75 <zf.zoodle@gmail.com>
2021-05-03 22:01:41 -0700

    Do while loop

--- while.cpp
+++ while.cpp
@@ -4,11 +4,18 @@ using namespace std;
 
 int main ()
 {
-  int index = 1;
+  int index = 6;
+
+  //checks condition first
   while(index <= 5) {
     cout << index << endl;
     index++;
   }
+  //does once then checks condition
+  do{
+    cout << index << endl;
+    index++;
+  } while(index <=5);
 
   return 0;
 }
