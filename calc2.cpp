9e704b5178c274604fdc57cf65e6ca8139c42d24
ZoeyF75 <zf.zoodle@gmail.com>
2021-05-03 21:18:54 -0700

    Second calculator

---
+++ calc2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int num1, num2;
+  char op;
+
+  cout << "Enter first numer: ";
+  cin >> num1;
+  cout << "Enter operator ";
+  cin >> op;
+  cout << "Enter second numer: ";
+  cin >> num2;
+
+  int result;
+  if (op == '+') {
+    result = num1 + num2;
+  } else if (op == '-') {
+    result = num1 - num2;
+  } else if (op == '*') {
+    result = num1 * num2;
+  } else if (op == '/') {
+    result = num1 / num2;
+  } else {
+    cout << "invalid operator";
+  }
+
+  cout << result << endl;
+
+  return 0;
+}
