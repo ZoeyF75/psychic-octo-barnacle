1241b44dff794325f094ed809ca05b044bc9f6e6
ZoeyF75 <zf.zoodle@gmail.com>
2021-05-05 15:06:26 -0700

    Working with memory adresses/pointers

---
+++ pointers.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+  int age = 19;
+  int *pAge = &age;
+  double gpa = 2.7;
+  double *pGpa = &gpa;
+  string name = "Mike";
+  string *pName = &name;
+
+  //prints out memory adress where age is stored
+  //these are known as pointers
+  cout << "Age: " << &age << endl;
+  cout << "GPA: " << &gpa << endl;
+  cout << "Name: " << &name << endl;
+
+  //uses pointer variables
+  cout << "Age: " << pAge << endl;
+  cout << "GPA: " << pGpa << endl;
+  cout << "Name: " << pName << endl;
+
+  return 0;
+}
