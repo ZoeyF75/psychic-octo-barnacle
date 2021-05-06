f4caa3dbacca6fdd68cffad2bb54a7e9611906e6
ZoeyF75 <zf.zoodle@gmail.com>
2021-05-06 00:12:19 -0700

    Functions for objects in class definitions

---
+++ obj-functions.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+class Student {
+  public:
+    string name;
+    string major;
+    double gpa;
+
+    Student(string aName, string aMajor, double aGpa) {
+      name = aName;
+      major = aMajor;
+      gpa = aGpa;
+    }
+
+    //for this scenario 3.5 or greater is honours
+    bool hasHonors() {
+      return gpa >= 3.5;
+    }
+};
+
+int main ()
+{
+  Student student1("Jim", "Business", 2.4);
+  Student student2("Pam", "Art", 3.6);
+
+  cout << student1.hasHonors() << endl; // 0 == false
+  cout << student2.hasHonors() << endl; // 1 == true
+
+
+  return 0;
+}
