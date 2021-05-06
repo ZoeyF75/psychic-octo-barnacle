092b3b2989b557a6010e9c68503c5ba915786172
ZoeyF75 <zf.zoodle@gmail.com>
2021-05-05 23:49:48 -0700

    Final commit before constructors

--- classes-objects.cpp
+++ classes-objects.cpp
@@ -7,16 +7,19 @@ class Book {
     string title;
     string author;
     int pages;
+    Book(string name) {
+      cout << "Creating Object " << name << endl;
+    }
 };
 
 int main ()
 {
-  Book book1;
+  Book book1("Harry Potter"); //Creating Object prints
   book1.title = "Harry Potter";
   book1.author = "J.K. Rowling";
   book1.pages = 500;
 
-  Book book2;
+  Book book2("LOTR"); //Creating Object prints
   book2.title = "Lord of the Rings";
   book2.author = "Tolkein";
   book2.pages = 700;
