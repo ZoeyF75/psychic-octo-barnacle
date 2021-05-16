7bb3471d2f2dd676a36f6e633c908aec116b6edc
ZoeyF75 <zf.zoodle@gmail.com>
2021-05-16 14:30:13 -0600

    Prints out teachers with longest name from 2d array

--- katas/kata4.cpp
+++ katas/kata4.cpp
@@ -3,10 +3,16 @@
 using namespace std;
 
 //change first number if more than 4 instructors, 2 shouldn't have to change
-string* instructorWithLongestName(string arr[4][2], int numOfInstructors) {
+string* instructorWithLongestName(string arr[4][2], int numOfInstructors, string longestName[]) {
+  longestName[0] = arr[0][0];
+  longestName[1] = arr[0][1];
   for (int i = 0; i < numOfInstructors; i++) {
-    cout << arr[i][0] << endl;
+    if (arr[i][0].length() > longestName[0].length()) {
+      longestName[0] = arr[i][0];
+      longestName[1] = arr[i][1];
+    }
   }
+  return longestName;
 }
 
 //In this exercise, we will be given a list of instructors and have to determine 
@@ -26,8 +32,15 @@ int main ()
     {"Domascus", "Web"}
   };
 
-  string *instructorAnswerOne = instructorWithLongestName(instructorsTestOne, 4);
-  string *instructorAnswerTwo = instructorWithLongestName(instructorsTestTwo, 3);
+  string longestName[2]; //sets longest name to first one
+  string *instructorAnswerOne = instructorWithLongestName(instructorsTestOne, 4, longestName);
+  cout << "name: " << instructorAnswerOne[0]<< ", course: " << instructorAnswerOne[1] << endl;
+
+  longestName[0] = "";
+  longestName[1] = "";
+  string *instructorAnswerTwo = instructorWithLongestName(instructorsTestTwo, 3, longestName);
+
+  cout << "name: " << instructorAnswerTwo[0] << ", course: " << instructorAnswerTwo[1] << endl;
 
   return 0;
 }
