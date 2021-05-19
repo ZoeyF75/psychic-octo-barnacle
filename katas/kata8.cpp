b821626357f2e27f5173b0961585383c8b31b42f
ZoeyF75 <zf.zoodle@gmail.com>
2021-05-19 11:57:01 -0700

    setup

---
+++ katas/kata8.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+string* repeatNumbers() {
+  
+}
+
+int main ()
+{
+  int array[1][2] = { 
+    {1, 10}
+  };
+
+  int array2[2][2] = {
+    {1, 2},
+    {2, 3}
+  };
+
+  int array3[3][2] = {
+    {10, 4},
+    {34, 6},
+    {92, 2}
+  };
+
+
+  // expected outputs
+  // 1111111111
+  // 11, 222
+  // 10101010, 343434343434, 9292
+  
+  return 0;
+}
