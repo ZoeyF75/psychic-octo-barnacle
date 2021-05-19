8bc6e8b8627250d6cf6056a5ecdcbcfda16bcb6b
ZoeyF75 <zf.zoodle@gmail.com>
2021-05-19 11:25:59 -0700

    Returns parking spot

--- katas/kata6.cpp
+++ katas/kata6.cpp
@@ -2,10 +2,32 @@
 
 using namespace std;
 
-int* whereCanIPark (char parkinglot[6][6], string vehicle) {
-  
+int* whereCanIPark (char parkinglot[6][6], string vehicle, int outerLength, int innerLength, int spot[]) {
+  for (int i = 0; i < outerLength; i++) {
+    for (int j = 0; j < innerLength; j++) {
+      if (vehicle == "motorcycle") {
+        if (parkinglot[i][j] == 'M' || parkinglot[i][j] == 'S' || parkinglot[i][j] == 'R') {
+          spot[0] = i;
+          spot[1] = j;
+          return spot;
+        }
+      } else if (vehicle == "small") {
+        if (parkinglot[i][j] == 'S' || parkinglot[i][j] == 'R') {
+          spot[0] = i;
+          spot[1] = j;
+          return spot;
+        }
+      } else if (vehicle == "regular") {
+        if (parkinglot[i][j] == 'R') {
+          spot[0] = i;
+          spot[1] = j;
+          return spot;
+        }
+      }  
+    }
+  }
+  return spot;
 }
-
 // There are three kinds of possible vehicles: regular cars, small cars, and motorcycles.
 // Regular cars can only park in R spots.
 // Small cars can park in R or S spots.
@@ -14,6 +36,8 @@ int* whereCanIPark (char parkinglot[6][6], string vehicle) {
 // An upper-case letter means that the particular spot is AVAILABLE, while lower-case letters mean that the spot is UNAVAILABLE.
 int main ()
 {
+  int spot[2] = {-1, -1};
+
   char lotOne[6][6] = {
     //COLUMNS ARE X
     // 0    1    2    3    4    5
@@ -25,7 +49,10 @@ int main ()
     {'S', 'r', 'S', 'M', 'M', 'S'}  //5
   };
 
-  cout << whereCanIPark(lotOne, "regular") << endl; 
+  spot[0] = -1;
+  spot[1] = -1;
+  int *spotOne = whereCanIPark(lotOne, "regular", 6, 6, spot); 
+  spotOne[0] == -1 ? cout << "false" << endl : cout << spotOne[1] << ',' << spotOne[0] << endl;
   
   char lotTwo[6][6] = {
     //COLUMNS ARE X
@@ -36,7 +63,10 @@ int main ()
     {'M', 'M', 'r', 'M'}, //3
   };
 
-  cout << whereCanIPark(lotTwo, "small") << endl; 
+  spot[0] = -1;
+  spot[1] = -1;
+  int *spotTwo = whereCanIPark(lotTwo, "small", 4, 4, spot); 
+  spotTwo[0] == -1 ? cout << "false" << endl : cout << spotTwo[1] << ',' << spotTwo[0] << endl;
 
   char lotThree[6][6] = {
     //COLUMNS ARE X
@@ -49,7 +79,8 @@ int main ()
     {'S', 'r', 'S', 'M', 'm', 'S'}  //5
   };
 
-  cout << whereCanIPark(lotThree, "motorcycle") << endl; 
+  int *spotThree = whereCanIPark(lotThree, "motorcycle", 6, 6, spot); 
+  spotThree[0] == -1 ? cout << "false" << endl : cout << spotThree[1] << ',' << spotThree[0] << endl;
    
   return 0;
 }
