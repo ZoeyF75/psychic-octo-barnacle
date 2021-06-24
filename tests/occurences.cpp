cba1b4761064665ad2ce4a7daa93b1e3f6f00aa0
ZoeyF75 <zf.zoodle@gmail.com>
2021-06-24 12:06:09 -0700

    efficient solution

--- tests/occurences.cpp
+++ tests/occurences.cpp
@@ -1,41 +1,25 @@
-#include <set>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 
-vector <int> numbers (vector<int> &A) {
-    vector<int> nums;
-    int length = A.size();
-    set<int> myset = {};   
-
-    for (int i = 0; i < length; i++) {
-        myset.insert(A[i]);
-    }
-
-    for (auto elem : myset){
-        nums.push_back(elem);
-    }
-
-    return nums;
-}
-
-int count (vector<int> &set, vector<int> &A) {
-    int lengthV = A.size();
-    int lengthS = set.size();
-    int occurences = 0;
-    for (int i = 0; i < lengthS; i++) {
-        for (int j = 0; j < lengthV; j++) {
-         if (A[j] == set[i]) {
-             occurences++;
-         }
-        }
-        if (occurences % 2 != 0) {
-            return set[i];
-        }
-        occurences = 0;
-    }
-    
-    return 0;
-}
+using namespace std;
 
 int solution(vector<int> &A) {
-    vector<int> nums = numbers(A);
-    return count(nums, A);
+    sort(A.begin(), A.end()); //sort from first element to last element
+    int length = A.size();
+    for(int i = 0; i < length; i+=2) {
+        if(A[i] != A[i+1]) return A[i];
+    }
+  return 0; //only returns if vector given is invalid
+}
+
+int main () {
+  vector<int> sample;
+  sample.push_back(2);
+  sample.push_back(1);
+  sample.push_back(1);
+  sample.push_back(7);
+  sample.push_back(2);
+
+  cout << solution(sample) << endl; //return 7
 }
